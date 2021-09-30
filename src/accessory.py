600cec0701d75648243c7bf8818790cc6782ce3b
Oscar Wellner <oscar.wellner@gmail.com>
2021-09-30 15:40:00 +0200

    Add properties to better describe Items

--- src/accessory.py
+++ src/accessory.py
@@ -1,9 +1,33 @@
+from enum import Enum
+
 from item import Item
+
+
+class AccessoryType(Enum):
+    CRATE = 'Crate'
+    LOCK = 'Lock'
+    BELL = 'Bell'
+    CLOTHING = 'Clothing'
+    BOTTLE = 'Bottle'
+    LUGGAGE_CARRIER = 'Luggage Carrier'
+
+
+def parse_accessory_type(value: any) -> AccessoryType:
+    if type(value) == AccessoryType:
+        return value
+
+    if type(value) is not str:
+        raise TypeError('Unexpected type error: Only pass AccessoryType or String values.')
+
+    for accessoryType in AccessoryType:
+        if accessoryType.value == value:
+            return accessoryType
 
 
 class Accessory(Item):
     _name: str = None
     _protective: bool = False
+    _accessory_type: AccessoryType = None
 
     def __init__(self, arguments: dict = None):
         if arguments is None:
@@ -14,12 +38,14 @@ class Accessory(Item):
         self.is_rented = arguments['rented']
         self.name = arguments['name']
         self.is_protective = arguments['protective']
+        self.accessory_type = arguments['accessory_type']
 
     def as_dict(self):
         accessory_dict = super().as_dict()
 
         accessory_dict['name'] = self.name
         accessory_dict['protective'] = self.is_protective
+        accessory_dict['accessory_type'] = self.accessory_type.value
 
         return accessory_dict
 
@@ -38,3 +64,11 @@ class Accessory(Item):
     @is_protective.setter
     def is_protective(self, protective: bool) -> None:
         self._protective = protective
+
+    @property
+    def accessory_type(self) -> AccessoryType:
+        return self._accessory_type
+
+    @accessory_type.setter
+    def accessory_type(self, accessory_type: any) -> None:
+        self._accessory_type = parse_accessory_type(accessory_type)
--- src/bike.py
+++ src/bike.py
@@ -1,10 +1,56 @@
+from enum import Enum
+
 from item import Item
 from person import Gender, parse_gender
+
+
+class Color(Enum):
+    BLACK = 'Black'
+    ORANGE = 'Orange'
+    RED = 'Red'
+    SILVER = 'Silver'
+    YELLOW = 'Yellow'
+    BLUE = 'Blue'
+    GREEN = 'Green'
+    LIME = 'Lime'
+
+
+def parse_color(value: any) -> Color:
+    if type(value) == Color:
+        return value
+
+    if type(value) is not str:
+        raise TypeError('Unexpected type error: Only pass Color or String values.')
+
+    for color in Color:
+        if color.value == value:
+            return color
+
+
+class Size(Enum):
+    SMALL = 'Small'
+    MEDIUM = 'Medium'
+    LARGE = 'Large'
+
+
+def parse_size(value: any) -> Size:
+    if type(value) == Size:
+        return value
+
+    if type(value) is not str:
+        raise TypeError('Unexpected type error: Only pass Size or String values.')
+
+    for size in Size:
+        if size.value == value:
+            return size
 
 
 class Bike(Item):
     _build_for_gender: Gender = None
     _electric: bool = False
+    _color: Color = None
+    _size: Size = None
+    _model: str = None
 
     def __init__(self, arguments: dict = None):
         if arguments is None:
@@ -15,12 +61,18 @@ class Bike(Item):
         self.is_rented = arguments['rented']
         self.build_for_gender = arguments['build_for_gender']
         self.is_electric = arguments['electric']
+        self.color = arguments['color']
+        self.size = arguments['size']
+        self.model = arguments['model']
 
     def as_dict(self):
         bike_dict = super().as_dict()
 
         bike_dict['build_for_gender'] = self.build_for_gender.value
         bike_dict['electric'] = self.is_electric
+        bike_dict['color'] = self.color.value
+        bike_dict['size'] = self.size.value
+        bike_dict['model'] = self.model
 
         return bike_dict
 
@@ -39,3 +91,27 @@ class Bike(Item):
     @is_electric.setter
     def is_electric(self, electric: bool) -> None:
         self._electric = electric
+
+    @property
+    def color(self) -> Color:
+        return self._color
+
+    @color.setter
+    def color(self, color: any) -> None:
+        self._color = parse_color(color)
+
+    @property
+    def size(self) -> Size:
+        return self._size
+
+    @size.setter
+    def size(self, size: any) -> None:
+        self._size = parse_size(size)
+
+    @property
+    def model(self) -> str:
+        return self._model
+
+    @model.setter
+    def model(self, model: str) -> None:
+        self._model = model
