455e2592d7938421eb61ea22b1bdce3d61bdb999
Oscar Wellner <oscar.wellner@gmail.com>
2021-11-07 17:13:49 +0100

    Add pyDoc in main file

--- isid/main.py
+++ isid/main.py
@@ -1,3 +1,10 @@
+"""
+Project created by Oscar Wellner, 21144192
+
+This application will read the CSV-files when the button on the main tab is pressed and insert the
+found data into the Text widgets of the overview tabs of the corresponding entities.
+"""
+
 from src.layout import BikerManagementApp
 
 
