50d3fb028b4e4ce2f2063f53aa9b1ad4cb96673a
Oscar Wellner <oscar.wellner@gmail.com>
2021-11-21 22:08:46 +0100

    Switch from Text to Treeview widget

--- isid/src/layout/overviewFrame.py
+++ isid/src/layout/overviewFrame.py
@@ -1,16 +1,20 @@
 from os import getcwd
 from os.path import join
-from tkinter import Text, PhotoImage
-from tkinter.constants import BOTH, SOLID, END, DISABLED, NORMAL
+from tkinter import PhotoImage
+from tkinter.constants import BOTH, END, YES, W
 from tkinter.font import BOLD, Font
-from tkinter.ttk import Frame, Label, Notebook
+from tkinter.ttk import Frame, Label, Notebook, Treeview
+
+from ..models import Item
 
 KEY_HEADER_TEXT = 'header_text'
-WIDTH_OVERVIEW = 136
+KEY_HEADERS = 'table_headers'
+KEY_OBJECT_TYPE = 'object_type'
 
 
 class OverviewFrame:
     def __init__(self, notebook: Notebook, **kwargs):
+        print(f"Initializing Overview Frame for {kwargs.get(KEY_HEADER_TEXT).replace(' Overview', '')}")
         self._frame = Frame(notebook)
 
         # Create a smaller logo
@@ -27,62 +31,40 @@ class OverviewFrame:
         header.grid(row=1, column=1, columnspan=2, ipady=8)
 
         # Create a Text widget for showing the data
-        self._overview = Text(self.frame, borderwidth=1, relief=SOLID)
-        self.overview.config(width=WIDTH_OVERVIEW)
-        self._overview.grid(row=2, column=1, columnspan=2)
+        headers = kwargs.get(KEY_HEADERS)
+
+        self._overview = Treeview(self.frame, columns=headers)
+        self._overview.grid(row=2, column=1, columnspan=2, padx=64)
 
         self.frame.grid_columnconfigure(2, weight=1)
-        self.overview.config(state=DISABLED)
+
+        is_item = isinstance(kwargs.get(KEY_OBJECT_TYPE)(), Item)
+        self._first_column = 'Brand' if is_item else 'Firstname'
+
+        headers = tuple([self._first_column]) + headers
+
+        # Create table columns and headers
+        for idx, header in enumerate(headers):
+            self.overview.heading(f'#{idx}', text=header, anchor=W)
+            self.overview.column(f'#{idx}', anchor=W, minwidth=64, stretch=YES)
 
         self.frame.pack(fill=BOTH, expand=True)
 
     def insert_data(self, objects):
         """
-        Inserts data into the Text Widget in a table like style
+        Inserts data into the Table widget
         :param objects: The data to insert
         """
-        self.overview.config(state=NORMAL)
-        headers = objects[0].attributes()
+        if len(objects) == 0:
+            return
 
-        # Insert the entity attributes as headers.
-        formatted_headers = self._format_line(headers)
-        self.overview.insert(END, formatted_headers)
+        for index, entry in enumerate(objects):
+            print(f'Inserting {type(entry).__name__} data into overview Treeview widget: {entry.values()}')
+            column_values = entry.values()
 
-        # Insert a line between the headers and the actual data
-        line = '-' * WIDTH_OVERVIEW + '\n'
-        self.overview.insert(END, line)
+            self.overview.insert('', index=END, iid=index, text=column_values.pop(0), values=tuple(column_values))
 
-        # Insert every entity in Text widget
-        for entry in objects:
-            formatted_entry = self._format_line(entry.values())
-            self.overview.insert(END, formatted_entry)
-
-        self.overview.config(state=DISABLED)
-
-    def _format_line(self, data) -> str:
-        """
-        Takes the data a creates a formatted String representation of it.
-        :param data: Data that needs to be formatted into a single line
-        :return: A single line ready to be inserted into the Text widget
-        """
-        # Determine how many items there needs to be on one line
-        number_of_items = len(data)
-
-        # Determine how many space a single item takes
-        space_per_item = (WIDTH_OVERVIEW // number_of_items) - 2
-        formatted_line = ''
-
-        # Add all values onto the formatted line separating them with a pipe `|`
-        for idx in range(0, len(data)):
-            entry = data[idx]
-
-            formatted_line += f'{str(entry).center(space_per_item)}'
-
-            # Don't add a pipe after the last value
-            if idx is not len(data) - 1:
-                formatted_line += ' |'
-
-        return formatted_line + '\n'
+        print()
 
     @property
     def frame(self):
